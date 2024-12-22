3aa6071248ed508125fe24ace812d8533f1ddc02
Theerroreng <theerror395@gmail.com>
2024-12-23 00:28:55 +0100

    Add files via upload

---
+++ MEUSITE2.js
@@ -0,0 +1,9 @@
+    setTimeout(function(){
+      document.getElementById('wel').style.display=" none";
+      document.getElementById('mae').style.display="block";
+      document.body.style="min-height:100vh;background-image: url('/70314397883674638-10884426570576108.jpg'); background-size: cover;background-repeat: no-repeat;min-height: 100vh;background-attachment: fixed";
+    },3000);
+      setTimeout(function() {
+        document.getElementById('falsebox').style.display = "none";
+        document.getElementById('caixas').style.display = "flex"; // Flex para centralização
+      }, 6100);
